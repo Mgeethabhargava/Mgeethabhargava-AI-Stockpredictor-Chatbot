629559991042ffae76323ad465eee1415feaeadb
Mgeethabhargava <m.gbhargava@gmail.com>
2024-12-05 17:14:42 +0530

    Revert "Removed LFS tracking and added actual file"
    This reverts commit 977fe3af8ebaa7736c3663bc3968a92e10aa10e7.

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ app = Flask(__name__)
 # Get the current script directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 # Load the pre-trained AI model
-MODEL_PATH = os.path.join(current_dir, 'AI_stock_model.h5')
-model_save_path = os.path.join(current_dir, 'multi_intent_model.pt')
+MODEL_PATH = os.path.join(current_dir, 'aimodels\AI_stock_model.h5')
+model_save_path = os.path.join(current_dir, 'aimodels\multi_intent_model.pt')
 model = load_saved_model(MODEL_PATH)
 intent_model, tokenizer, mlb, device = multimodel(model_save_path)
 # Create a directory for saving graph images
